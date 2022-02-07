d99e9496a5c5d03be28efb2a275b025991272cf7
Nikko10Have <tenhavenikko48@gmail.com>
2022-02-07 15:57:03 +0000

    Fixed Commentation on test_geo

--- test_geo.py
+++ test_geo.py
@@ -4,8 +4,8 @@ from floodsystem.geo import *
 from floodsystem.stationdata import build_station_list
 import random
 
-
 # TASK 1B: Check that the outputs are of the correct type and value
+
 
 def test_stations_by_distance():
     """Check that the outputs of stations_by_distance are of the correct type and value"""
@@ -14,17 +14,17 @@ def test_stations_by_distance():
     p = (52.2053, 0.1218)
     X = stations_by_distance(stations, p)
 
-    # The output must be a list
+    # The output must be list
     if type(X) != List:
         raise TypeError("Output is not a list")
 
-    # Every entry in the list must be a tuple
+    # Every entry must be tuple
     for entry in X:
         if type(entry) != Tuple:
             raise TypeError("At least one entry in the list is not a tuple")
             break
 
-    # Check that the list orders each station by distance
+    # Ordered by distance
     for n in range(0, len(X) - 1):
         assert X[n + 1][1] >= X[n][1]
 
@@ -36,20 +36,19 @@ def test_stations_within_radius():
     stations = build_station_list()
     p = (52.2053, 0.1218)
 
-    # We know that there are no stations within a radius of 0
+    # No stations within a radius of 0
     assert stations_within_radius(stations, p, 0) == []
 
-    # We know that for any radius, the output list must be in alphabetical order
     # Choose a random radius
     R = random.randint(5, 10000)
 
-    # Obtain output list
+    # Output list
     X = stations_within_radius(stations, p, R)
 
-    # Check the type of the output
+    # Type of the output
     assert type(X) == list
 
-    # Check that the list is in alphabetical order
+    # Alphabetical order
     for n in range(0, len(X) - 1):
         assert X[n + 1] >= X[n]
 
@@ -62,14 +61,14 @@ def test_rivers_with_station():
     # Obtain output
     X = rivers_with_station(stations)
 
-    # Check that the output is of the correct type
+    # Output is of correct type
     assert type(X) == Set
 
-    # Check that every entry in the set is of the correct type
+    # Type of every entry in the set is correct
     for n in range(0, len(X) - 1):
         assert type(X[n]) == String
 
-    # The output should not contain duplicate entries
+    # No duplicate entries
     for entry in X:
         if X.count(entry) > 1:           # Frequency Counter
             raise ValueError("There are duplicate entries in the output")
@@ -82,9 +81,9 @@ def test_stations_by_river():
     # Obtain output
     X = stations_by_river(stations)
 
-    # Check the output type
+    # Output type
     assert type(X) == dict
-    # We know that the River Cam is in Cambridge
+    # River Cam is in Cambridge
     assert "Cambridge" in X["River Cam"]
 
 # TASK 1E: Check that the outputs are of the correct type and are ordered properly
@@ -97,11 +96,11 @@ def test_rivers_by_station_number():
     N = random.randint(1, 1000)
     X = rivers_by_station_number(stations, N)
 
-    # Check the type of the output
+    # Type of the output
     assert type(X) == list
 
-    # Check that the list is in alphabetical order
+    # Alphabetical order
     for n in range(0, len(X) - 1):
         assert X[n + 1][1] >= X[n][1]
-    # Check that the list is of length N or greater
+    # List is of length N or greater
     assert len(X) >= N
